3aab222873042b6853e7412609208caa2b1e0d9a
Federico Gasquez <federicogasquez@gmail.com>
2020-03-22 23:07:37 -0300

    fix

--- flaskps/decorators/auth.py
+++ flaskps/decorators/auth.py
@@ -3,6 +3,5 @@ from functools import wraps
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        raise Exception("LA CONCHA DEL CREADOR DE FLASK")
-        return "sarasa"
+        return "test"
     return decorated_function
