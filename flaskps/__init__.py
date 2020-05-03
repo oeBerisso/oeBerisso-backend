e350f72ef5a45bbe18a14b0357723b416c26b340
Federico Gasquez <federicogasquez@gmail.com>
2020-05-03 19:30:30 -0300

    config google auth

--- flaskps/__init__.py
+++ flaskps/__init__.py
@@ -7,8 +7,14 @@ from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
 )
+from oauthlib.oauth2 import WebApplicationClient
 
 app = Flask(__name__)
+
+app.secret_key = Config.GOOGLE_CLIENT_SECRET
+# OAuth 2 client setup
+client = WebApplicationClient(Config.GOOGLE_CLIENT_ID)
+
 
 # Setup the Flask-JWT-Extended extension
 app.config['JWT_SECRET_KEY'] = 'sdlkjhghsgfinjpjaSOJIdSFOJSAdKJFA1'
@@ -18,6 +24,10 @@ jwt = JWTManager(app)
 
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+@app.route("/glogin")
+def glogin():
+    return auth.google_login(client)
 
 @app.route("/api/v1.0/auth", methods=["POST"])
 def login():
--- flaskps/resources/auth.py
+++ flaskps/resources/auth.py
@@ -1,6 +1,24 @@
-from flask import jsonify, session
+from flask import jsonify, session, request, redirect
 from flaskps.db import get_db
 from flaskps.models.user import User
+from flaskps.config import Config
+
+import requests
+
+def google_login(client):
+    # Find out what URL to hit for Google login
+    google_provider_cfg = requests.get(Config.GOOGLE_DISCOVERY_URL).json()
+
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
 
 
 def login(request, create_access_token):
