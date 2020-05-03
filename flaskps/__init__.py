f68f808e55332c061b906eb0e1952d7efe071042
vanza <agustin.vanza@gmail.com>
2020-05-03 19:28:04 -0300

    added cors and new endpoint

--- flaskps/__init__.py
+++ flaskps/__init__.py
@@ -1,17 +1,22 @@
-from flask import Flask, flash, request
+from flask import Flask, flash, request, url_for, jsonify
 from flask_session import Session
 from flaskps.config import Config
 from flaskps.decorators.auth import login_required
 from flaskps.resources import auth
+from flask_cors import CORS
+
 from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
+    JWTManager,
+    jwt_required,
+    create_access_token,
+    get_jwt_identity,
 )
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 # Setup the Flask-JWT-Extended extension
-app.config['JWT_SECRET_KEY'] = 'sdlkjhghsgfinjpjaSOJIdSFOJSAdKJFA1'
+app.config["JWT_SECRET_KEY"] = "sdlkjhghsgfinjpjaSOJIdSFOJSAdKJFA1"
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config.from_object(Config)
 jwt = JWTManager(app)
 
@@ -19,11 +24,38 @@ jwt = JWTManager(app)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# routes
+app.add_url_rule("/api/v1.0/register", "user_create", auth.create, methods=["POST"])
+
+
 @app.route("/api/v1.0/auth", methods=["POST"])
 def login():
     return auth.login(request, create_access_token)
+
 
 @app.route("/api/v1.0/")
 @login_required
 def home():
     return "hello word"
+
+@app.route("/")
+def test():
+    return "<h1>hello world</h1>"
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/routes")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return jsonify({"url": links})
--- flaskps/resources/auth.py
+++ flaskps/resources/auth.py
@@ -1,6 +1,17 @@
-from flask import jsonify, session
+from flask import jsonify, session, request
 from flaskps.db import get_db
 from flaskps.models.user import User
+from flaskps.helpers import form_validator
+from flaskps.validations import register
+
+def create():
+    if not form_validator.validate(register.rules, request.json):
+        User.db = get_db()
+        User.create(request.json)
+        return jsonify({"response":"todo pillo"}), 200
+    else:
+        return jsonify({"response":"toca de aca"}), 422
+
 
 
 def login(request, create_access_token):
@@ -18,9 +29,14 @@ def login(request, create_access_token):
     if status and user["active"] == 0:
         msg = "El usuario no se encuentra activo."
         status = False
-    
+
     if status:
         msg = "La sesión se inició correctamente."
 
-
-    return jsonify({"status": status, msg: msg, "token": create_access_token(identity=user["username"]) })
+    return jsonify(
+        {
+            "status": status,
+            msg: msg,
+            "token": create_access_token(identity=user["username"]),
+        }
+    )
