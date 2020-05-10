3cf7e5d357f5ef7af30b045bb7ba15df3d0e569f
Federico Gasquez <federicogasquez@gmail.com>
2020-05-09 22:22:16 -0300

    fix env variables

--- flaskps/config/development.py
+++ flaskps/config/development.py
@@ -1,6 +1,15 @@
+import os
+
 SECRET_KEY = "dev"
 DEBUG = True
 DB_HOST = "localhost"
 DB_USER = "root"
 DB_PASS = "pass"
 DB_NAME = "grupo16"
+
+# Google Configuration
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
